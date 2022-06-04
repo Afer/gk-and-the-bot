c37bdde46af45dbc61ad3b8de96a3fc9d5fff83c
My Name <team@example.com>
2022-06-03 20:31:26 -0600

    router

--- script/main.js
+++ script/main.js
@@ -3,7 +3,16 @@ var ANIMATION_EVENT = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimat
 
 var main = {
     init: function() {
+        let path = window.location.pathname;
 
+        $(".pages").css("display", "none");
+
+        if (path == '' || path.indexOf('index') != -1 || path.indexOf('home') != -1) {
+            $(".home").css("display", "block");
+        }
+        else if (path.indexOf("privacy-policy") != -1) {
+            $(".privacy-policy").css("display", "block");
+        }
     }
 };
 
